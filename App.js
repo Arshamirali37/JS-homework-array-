ff8d53f9825a900c1a195398a6f7a2765bab8015
ARSHamirali <arshamirali604@gmail.com>
2024-09-26 14:49:47 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,146 @@
+//                      Home work
+
+//     Chapter#15
+// Q3
+// var name =["arsh","Amirali","Ali","Alishan"]
+
+// Q4
+// var number=[1,2,3,4,5]
+
+// Q5
+// var boolean=[ true,false]
+
+// Q6
+// var mixture=["Arsh",1,true]
+
+// Q7
+// var educationqualificationsinPakistan=["SSC ", "HSC", "BCS",
+//     "BS", "BCOM", "MS", "MPhil" , "PhD"]
+//     document.write(educationqualificationsinPakistan)
+
+// Q8
+// var students=["ARSH","ALI","MUHAMMAD"]
+// var marks=[320,240,280]
+// var percentage=["64%" ,"48%" , "56%"]
+// document.write(`Score of ${students[0]} is ${marks[0]} percentage is ${percentage[0]} <br>
+//     Score of ${students[1]} is ${marks[1]} percentage is ${percentage[1]} <br>
+//     Score of ${students[2]} is ${marks[2]} percentage is ${percentage[2]} <br>`)
+
+// Q9(a)
+// var Color=["RED","GREEN","BLUE"]
+// var userchoice=prompt("Enter your favourit Color..")
+// var Colorbychoice;
+// alert(`you have choice ${userchoice} color great choice!!`)
+
+// (b)
+// var Color=["RED","GREEN","BLUE"]
+// var userchoice=prompt("Enter your favourite Color..")
+// color=userchoice.push(userchoice)
+
+// (c)
+// var Color=["RED","GREEN","BLUE"]
+// Color.unshift("PURPLE","YELLOW")
+// document.write(Color)
+
+// (d)
+// var Color=["RED","GREEN","BLUE"]
+// Color.shift()
+// document.write(Color)
+
+// (e)
+// var Color=["RED","GREEN","BLUE"]
+// Color.pop()
+// document.write(Color)
+
+// (f)
+// var Color=["RED","GREEN","BLUE"]
+// Color.shift()
+// document.write(Color)
+
+//(g)
+// var Color=["RED","GREEN","BLUE"]
+// var userchoice=prompt("Which Color do you want to add & at which index?")
+// Color.splice(1,2,userchoice)
+// document.write(Color)
+
+//(h)
+// var Color=["RED","GREEN","BLUE"]
+// var userchoice=prompt("Which Color do you want to add & at which index?")
+// Color.pop(userchoice)
+// document.write(Color)
+
+// Q10
+// need help
+
+// Q11
+// var cities =["islamabad","quetta","Karachi","lahore"]
+// var selectedcity=[cities[1],[3]]
+
+// Q12
+// var arr = ["This", "is","my","cat"];
+// document.write(arr[1]," ",arr[2]," ",arr[3]," ",arr[4])
+
+// Q15
+// var Mobile = ["Samsung", "Haier", "Sony", "Nokia", "Motorla", "Apple"]
+// document.write(`<select>
+// <option value="">${Mobile[0]}</option>
+// <option value="">${Mobile[1]}</option>
+// <option value="">${Mobile[2]}</option>
+// <option value="">${Mobile[3]}</option>
+// <option value="">${Mobile[4]}</option>
+// <option value="">${Mobile[5]}</option>
+// </select>`)
+
+// Q16
+// var empty=[]
+
+// Q17
+// var empty=["Arsh"]
+
+// Q18
+// var alphabet = ["h","i","j","k"]
+// alert(alphabet[2])
+
+// Q19
+// var alphabet = ["h","i","j","k","m","n","o"]
+// console.log(alphabet.length)
+
+// Q20
+// var alphabet = ["h","i","j","k"]
+// alphabet.push("a")
+// console.log(alphabet)
+
+//      Chapter#16
+
+// Q1
+// var alphabet = ["h","i","j","k"]
+// alphabet.push("a")
+// console.log(alphabet)
+
+// Q2
+// var alphabet = ["h","i","j","k"]
+// alphabet.pop()
+// console.log(alphabet)
+
+// Q3
+// var alphabet = ["h","i","j","k"]
+// alphabet.push("2")
+// console.log(alphabet)
+
+// Q4
+// var sizes = ["S","M","XL","XXL","XXXL"]
+// sizes.shift()
+
+// Q5
+// var sizes = ["S","M","XL","XXL","XXXL"]
+// sizes.shift(0,3)
+
+// Q6
+// var one=["B"]
+// one.unshift("A")
+// alert(one)
+
+// Q7
+// var sizes = ["S","M","XL","XXL","XXXL"]
+// sizes.splice(1,2,"M")
+
